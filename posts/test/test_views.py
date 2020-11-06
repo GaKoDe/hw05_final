623fe53e0b5024830b0d71d989e9e4b8dcc746b0
Константин <colin1543@gmail.com>
2020-11-06 23:33:46 +0300

    Внёс изменения в тесты

--- posts/test/test_views.py
+++ posts/test/test_views.py
@@ -17,18 +17,17 @@ class StaticURLTests(TestCase):
         cls.user = User.objects.create_user(username=p['username'])
         cls.authorized_client = Client()
         cls.authorized_client.force_login(cls.user)
-
         cls.follower = User.objects.create_user(
             username=p['follower'])
         cls.authorized_client_follower = Client()
         cls.authorized_client_follower.force_login(cls.follower)
-
         cls.unfollower = User.objects.create_user(username=p['unfollower'])
         cls.authorized_client_unfollower = Client()
         cls.authorized_client_unfollower.force_login(cls.unfollower)
         cls.unauthorized_client = Client()
         cls.group = Group.objects.create(
             slug='test', title='test', description='test')
+        cls.post = Post.objects.create(text=p['text'], group=cls.group, author=cls.user)
 
     def check_urls(self, user, post, group):
         return [
@@ -39,10 +38,7 @@ class StaticURLTests(TestCase):
                     'group_posts', args=[group])]
 
     def test_posts(self):
-        response = self.authorized_client.post(
-            reverse("new_post"), {
-                'text': p['text'], 'group': self.group.pk},
-            follow=True)
+        response = self.authorized_client.get(reverse('post_view', args=[self.user,self.post.id]))
         self.assertEqual(response.status_code, 200)
         cache.clear()
         for page in self.check_urls(self.user, 1, self.group.slug):
@@ -52,22 +48,17 @@ class StaticURLTests(TestCase):
             self.assertContains(get_page_unauth, p['text'])
 
     def test_edit_post(self):
-        create_post = self.authorized_client.post(
-            reverse('new_post'), {
-                'text': p['text'], 'group': self.group.pk},
-            follow=True)
-        self.assertEqual(create_post.status_code, 200)
         new_gpoup = Group.objects.create(
             slug='new', title='new', description='new')
         response = self.authorized_client.post(
             reverse(
                 'post_edit', args=[
-                    self.user, 1]), {
+                    self.user, self.group.pk]), {
                 'text': p['edited_text'], 'group': new_gpoup.pk},
             follow=True)
         self.assertEqual(response.status_code, 200)
         cache.clear()
-        for page in self.check_urls(self.user, 1, new_gpoup.slug):
+        for page in self.check_urls(self.user, self.group.pk, new_gpoup.slug):
             get_page = self.authorized_client.get(page)
             get_page_unauth = self.unauthorized_client.get(page)
             self.assertContains(get_page, p['edited_text'])
@@ -82,19 +73,14 @@ class StaticURLTests(TestCase):
                 'image': uploaded},
             follow=True)
         self.assertEqual(response.status_code, 200)
-        get_img = Post.objects.get(pk=1)
+        get_img = Post.objects.get(pk=2)
         self.assertNotEqual(get_img.image, '')
 
     def test_page_for_img(self):
         main_page = reverse('index')
         uploaded = SimpleUploadedFile(
             'small.gif', p['small_gif'], content_type='image/gif')
-        response = self.authorized_client.post(
-            reverse("new_post"), {
-                'text': p['text'], 'group': self.group.pk,
-                'image': uploaded},
-            follow=True)
-        self.assertEqual(response.status_code, 200)
+        response = Post.objects.create(text=p['text'], group=self.group, author=self.user, image=uploaded)
         cache.clear()
         get_page = self.authorized_client.get(main_page)
         get_page_unauth = self.unauthorized_client.get(main_page)
@@ -104,14 +90,9 @@ class StaticURLTests(TestCase):
     def test_pages_for_img(self):
         uploaded = SimpleUploadedFile(
             'small.gif', p['small_gif'], content_type='image/gif')
-        response = self.authorized_client.post(
-            reverse("new_post"), {
-                'text': p['text'], 'group': self.group.pk,
-                'image': uploaded},
-            follow=True)
-        self.assertEqual(response.status_code, 200)
+        response = Post.objects.create(text=p['text'], group=self.group, author=self.user, image=uploaded)
         cache.clear()
-        for page in self.check_urls(self.user, 1, self.group.slug):
+        for page in self.check_urls(self.user, response.pk, self.group.slug):
             get_page = self.authorized_client.get(page)
             get_page_unauth = self.unauthorized_client.get(page)
             self.assertContains(get_page, '<img')
@@ -127,19 +108,17 @@ class StaticURLTests(TestCase):
     def test_index_cache(self):
         # Тест работает но я не понимаю почему так.
         # Спросить на ревью
-        self.client.get(reverse('index'))
-        self.client.post(reverse('new_post'), {'text': p['text']})
-        response = self.client.get(reverse('index'))
-        self.assertNotContains(response, p['text'])
+        with self.assertNumQueries(7):
+            response = self.authorized_client.get(reverse("index"))
+            self.assertEqual(response.status_code, 200)
+            response = self.authorized_client.get(reverse("index"))
+            self.assertEqual(response.status_code, 200)
 
     def test_unauth_comment(self):
-        response = self.authorized_client.post(
-            reverse("new_post"), {
-                'text': p['text'], 'group': self.group.pk},
-            follow=True)
+        response = self.authorized_client.get(reverse('post_view', args=[self.user,self.post.id]))
         self.assertEqual(response.status_code, 200)
         make_comment_response = self.unauthorized_client.get(
-            reverse("add_comment", args=[self.user, 1]))
+            reverse("add_comment", args=[self.user, self.post.pk]))
         self.assertRedirects(
             make_comment_response,
             reverse('login') +
@@ -148,18 +127,15 @@ class StaticURLTests(TestCase):
                 'add_comment',
                 args=[
                     self.user,
-                    1]),
+                    self.post.pk]),
             status_code=302,
             target_status_code=200)
 
     def test_auth_comment(self):
-        response = self.authorized_client.post(
-            reverse("new_post"), {
-                'text': p['text'], 'group': self.group.pk},
-            follow=True)
+        response = self.authorized_client.get(reverse('post_view', args=[self.user,self.post.id]))
         self.assertEqual(response.status_code, 200)
         make_comment_response = self.authorized_client.post(
-            reverse("add_comment", args=[self.user, 1]), {
+            reverse("add_comment", args=[self.user, self.post.pk]), {
                 'text': p['comment']},
             follow=True)
         self.assertContains(make_comment_response, p['comment'])
@@ -193,23 +169,18 @@ class StaticURLTests(TestCase):
         check_follower_in_db = check_follower_in_db = Follow.objects.filter(
             author__username__icontains=self.user,
             user__username__icontains=self.follower)
-        self.assertEqual(len(check_follower_in_db), 0)
+        self.assertEqual(check_follower_in_db.count(), 0)
 
     def test_follower(self):
-        response = self.authorized_client.post(
-            reverse("new_post"), {
-                'text': p['text'], 'group': self.group.pk},
-            follow=True)
-        self.assertEqual(response.status_code, 200)
-
-        response1 = self.authorized_client_follower.get(
+        response = self.authorized_client.get(reverse('post_view', args=[self.user,self.post.id]))
+        follow = self.authorized_client_follower.get(
             reverse("profile_follow", args=[self.user]))
-        self.assertRedirects(response1, reverse('profile', args=[self.user]),
+        self.assertRedirects(follow, reverse('profile', args=[self.user]),
                              status_code=302, target_status_code=200)
-        response2 = self.authorized_client_follower.get(
+        check_follow = self.authorized_client_follower.get(
             reverse('follow_index'))
-        self.assertContains(response2, p['text'])
+        self.assertContains(check_follow, p['text'])
         cache.clear()
-        response3 = self.authorized_client_unfollower.get(
+        un_follow = self.authorized_client_unfollower.get(
             reverse('follow_index'))
-        self.assertNotContains(response3, p['text'])
+        self.assertNotContains(un_follow, p['text'])
